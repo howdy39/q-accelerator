f8b3a84a8c099c98b9ed3a9be3f0022ef1c7a3e4
Tatsuya Nakano <admin@howdylikes.jp>
2016-12-23 19:41:47 +0900

    feat(設定画面): テーマ変更
    Close #78

--- src/components/settings/extend/copy-code.js
+++ src/components/settings/extend/copy-code.js
@@ -6,7 +6,7 @@ export default function (settings) {
   const component = Vue.extend({
     template: `<div class="mdl-card mdl-cell mdl-cell--12-col">
                  <div class="mdl-card__supporting-text">
-                   <h4>コードのコピー機能を追加</h4>
+                   <h4>コードのコピー機能</h4>
                    <p>
                      コードの上部にコピーアイコンを表示します。<br>
                      ※コピーは差分を考慮するため、削除行をコピーしません。また行頭の+(プラス)を除外します。
