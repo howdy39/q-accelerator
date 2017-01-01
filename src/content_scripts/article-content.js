d7016ad75116304d4a0cc8177554d7da0461d0d7
Tatsuya Nakano <admin@howdylikes.jp>
2017-01-01 19:39:54 +0900

    chore: 参照記事のミュートを分離

--- src/content_scripts/article-content.js
+++ src/content_scripts/article-content.js
@@ -1,6 +1,7 @@
 import Util from '../js/util';
 import AutoLikeContent from './article/auto-like-content.js';
 import CopyCodeContent from './article/copy-code-content.js';
+import MuteUserArticleContent from './article/mute-user-article-content.js';
 import MuteUserCommentContent from './article/mute-user-comment-content.js';
 import SaveHisotoryContent from './article/save-history.js';
 import ShowLineNumberContent from './article/show-line-number-content.js';
@@ -13,6 +14,14 @@ Util.getSettings(settings => {
   try {
     if (settings['auto-like'] || settings['auto-stock']) {
       new AutoLikeContent().run(settings['auto-like'], settings['auto-stock']);
+    }
+  } catch (e) {
+    Util.errorLog(e);
+  }
+
+  try {
+    if (settings['mute-users'] && settings['mute-user-article']) {
+      new MuteUserArticleContent().run(settings['mute-users']);
     }
   } catch (e) {
     Util.errorLog(e);
---
+++ src/content_scripts/article/mute-user-article-content.js
@@ -0,0 +1,19 @@
+import Util from '../../js/util';
+import ArticleDomHandler from './article-dom-handler.js';
+
+
+export default class MuteUserArticleContent {
+
+  run(muteUserIds) {
+    const handler = new ArticleDomHandler();
+
+    handler.getArticle().references.forEach(reference => {
+      const hasUserId = muteUserIds.includes(reference.userId);
+      if (hasUserId) {
+        handler.unShowReference(reference);
+        Util.infoLog(`特定ユーザーの投稿を非表示(${reference.userId})`, `${reference.title}の参照記事を非表示にしました`);
+      }
+    });
+  }
+
+}
