cff861afa460fe14ec269026faf9bdb76acda341
bells17 <bells171@gmail.com>
2017-01-12 15:02:12 +0900

    コードハイライトの文章末尾の改行数に合わせて正しい行番号が表示されるように修正

--- src/content_scripts/article/show-line-number-content.js
+++ src/content_scripts/article/show-line-number-content.js
@@ -9,8 +9,7 @@ export default class ShowLineNumberContent {
     const codeFrames = handler.getCodeFrames();
 
     for (let codeFrame of codeFrames) {
-      const rows = codeFrame.codeText.split('\n');
-      const length = rows.length;
+      const length = codeFrame.codeElement.textContent.split(/\n/).length - 1;
       let numbers = document.createElement('pre');
       numbers.className = 'qa-khsk-codeNumber';
       for (let i = 0; i < length; ++i) {
@@ -18,9 +17,6 @@ export default class ShowLineNumberContent {
 
         let label = document.createElement('span');
         label.innerHTML = number + '\n'; // Textでは改行が反映されない
-        if (i + 1 === length) {
-          label.innerHTML += '\n'; // 最終行には改行が追加されるqiitaの仕様に合わせる
-        }
         label.dataset.lineNumber = i + 1;
 
         numbers.appendChild(label);
